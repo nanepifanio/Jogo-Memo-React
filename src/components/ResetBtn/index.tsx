e9f459342139511c56972bcdc4f31dcb6c5cb6ff
Renan Epifânio <renan.epifanio@outlook.com>
2022-04-26 01:47:01 -0300

    index.tsx in ResetBtn updated

--- src/components/ResetBtn/index.tsx
+++ src/components/ResetBtn/index.tsx
@@ -14,31 +14,3 @@ export const ResetBtn = ({ resetClick }: Props) => {
     </Btn>
   );
 };
-
-/* let timer = null;
-  let seconds = 0;
-
-  const showTimer = (date) => {
-    cronometro.innerText = date.toLocaleTimeString("pt-BR", {
-      hour12: false,
-      timeZone: "UTC", // Ou 'GMT'
-    });
-  };
-
-  const updateTime = (sec) => {
-    const date = new Date(sec * 1000);
-    showTimer(date);
-  };
-
-  const iniciar = () => {
-    timer = setInterval(() => {
-      seconds++;
-      updateTime(seconds);
-    }, 1000);
-  };
-
-  const zerar = () => {
-    clearInterval(timer);
-    seconds = 0;
-    updateTime(seconds);
-  }; */
